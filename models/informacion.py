c0f2a6782368812db4b7509e4778330801b7362d
antonio <blackhorsef8@gmail.com>
2024-11-26 10:28:16 +0100

    26/11/24

--- models/informacion.py
+++ models/informacion.py
@@ -13,9 +13,20 @@ class informacion(models.Model):
     ancho_en_cms = fields.Integer(string="Ancho en cms:")
     longo_en_cms = fields.Integer(string="Longo en cms:")
     peso = fields.Float(digits=(6,2),default=2.7,string="Peso en KG.s:")
+    volume = fields.Float(digits=(6, 7), compute="_volume", store=True, string="Volume m3")
+    literal = fields.Char(store=False)
     autorizado = fields.Boolean(string="¿Autorizado?", default=True)
     sexo_traducido = fields.Selection([('Hombre','Home'),('Mujer','Muller'),('Otros','Outros')],string="Sexo:")
 
+    @api.depends('alto_en_cms', 'longo_en_cms', 'ancho_en_cms')
+    def _volume(self):
+        for rexistro in self:
+            rexistro.volume = float(rexistro.alto_en_cms) * float(rexistro.longo_en_cms) * float(rexistro.ancho_en_cms) / 1000000
 
-
-
+    @api.onchange('alto_en_cms')
+    def _avisoAlto(self):
+         for rexistro in self:
+             if rexistro.alto_en_cms > 7:
+                 rexistro.literal = 'O alto ten un valor posiblemente excesivo %s é maior que 7' % rexistro.alto_en_cms
+             else:
+                 rexistro.literal = ""
