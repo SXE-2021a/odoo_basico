8298d5ff9ee9339f0d6806cb2db7ee488507d347
antonio <blackhorsef8@gmail.com>
2025-02-20 14:16:23 +0100

    20/02/25

--- models/informacion.py
+++ models/informacion.py
@@ -2,6 +2,10 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+import os
+import pytz
+from . import miñasUtilidades
+import locale
 
 
 class informacion(models.Model):
@@ -34,9 +38,15 @@ class informacion(models.Model):
     moeda_dolar_id = fields.Many2one('res.currency',
                                     default=lambda self: self.env['res.currency'].search([('name', '=', "USD")],
                                                                                          limit=1))
-    gasto_en_dolares = fields.Monetary("Gasto en dolares", 'moeda_dolar_id')
-
     # con domain, filtramos os valores mostrados. Pode ser mediante unha constante (vai entre comillas) ou unha variable
+    gasto_en_dolares = fields.Monetary("Gasto en dolares", 'moeda_dolar_id')
+    data = fields.Date(string="Data", default=lambda self: fields.Date.today())
+    data_hora = fields.Datetime(string="Data e Hora", default=lambda self: fields.Datetime.now())
+    hora_utc = fields.Char(compute="_actualiza_hora_utc", string="Hora UTC", size=15, store=True)
+    hora_timezone_usuario = fields.Char(compute="actualiza_hora_timezone_usuario", string="Hora Timezone do Usuario",
+                                        size=15, store=True)
+    mes_castelan = fields.Char(compute="_mes_castelan", string="Mes en castelán", size=15, store=True)
+    mes_galego = fields.Char(compute="_mes_galego", string="Mes en galego", size=15, store=True)
 
     @api.depends('alto_en_cms', 'longo_en_cms', 'ancho_en_cms')
     def _volume(self):
@@ -84,3 +94,67 @@ class informacion(models.Model):
         mail_id = self.env['mail.mail'].create(mail_valores)
         mail_id.sudo().send()
         return True
+
+    def ver_contexto(self):  # Este metodo é chamado dende un botón de informacion.xml
+        for rexistro in self:
+            # Ao usar warning temos que importar a libreria mediante from odoo.exceptions import Warning
+            # Importamos tamén a libreria os mediante import os
+            # raise Warning(
+            #     'Contexto: %s Ruta: %s Contido %s' % (rexistro.env.context, os.getcwd(), os.listdir(os.getcwd())))
+            # # Warning is deprecated por iso usamos ValidationError
+
+            raise ValidationError('Contexto: %s Ruta: %s Contido do directorio %s' % (
+                rexistro.env.context, os.getcwd(), os.listdir(os.getcwd())))
+            # env.context é un diccionario  https://www.w3schools.com/python/python_dictionaries.asp
+        return True
+
+    def convirte_data_hora_de_utc_a_timezone_do_usuario(self,data_hora_utc_object):  # recibe a data hora en formato object
+        usuario_timezone = pytz.timezone(self.env.user.tz or 'UTC')  # obter a zona horaria do usuario. Ollo!!! nas preferencias do usuario ten que estar ben configurada a zona horaria
+        return pytz.UTC.localize(data_hora_utc_object).astimezone(usuario_timezone)  # hora co horario do usuario en formato object
+        # para usar  pytz temos que facer  import pytz
+
+    def chamado_dende_pedido_e_dende_apidepends(self,parametro_cos_datos_a_actualizar):  # Ten 2 parametros xa que polo segundo recibe os rexistros que queremos actualizar dende pedido
+        # TypeError: informacion.actualiza_hora_timezone_usuario() takes 1 positional argument but 2 were given
+        #     parametro_cos_datos_a_actualizar.hora_timezone_usuario = self.convirte_data_hora_de_utc_a_timezone_do_usuario(parametro_cos_datos_a_actualizar.data_hora).strftime("%H:%M:%S")  # Convertimos a hora de UTC a hora do timezone do usuario
+        for rexistro in parametro_cos_datos_a_actualizar:
+            rexistro.hora_timezone_usuario = rexistro.convirte_data_hora_de_utc_a_timezone_do_usuario(
+                rexistro.data_hora).strftime("%H:%M:%S")  # Convertimos a hora de UTC a hora do timezone do usuario
+
+    @api.depends('data_hora')
+    def _actualiza_hora_utc(self):
+        for rexistro in self: # A hora se almacena na BD en horario UTC (2 horas menos no verán, 1 hora menos no inverno)
+            rexistro.hora_utc = rexistro.data_hora.strftime("%H:%M:%S")
+
+
+    @api.depends('data_hora')
+    def actualiza_hora_timezone_usuario(self):  # Non pode ser un metodo privado porque da erro ao ser chamado dende o boton na vista xml        # "_actualiza_hora_timezone_usuario en odoo_basico.informacion es privado y no se puede activar con un botón"
+        for rexistro in self:
+              rexistro.chamado_dende_pedido_e_dende_apidepends(rexistro)  # leva rexistro como parametro por que chamado_dende_pedido_e_dende_apidepends ten 2 parametros
+
+    # Podemos  configurar locales a nivel de sistema con dpkg-reconfigure locales poñendo un por defecto.
+    # apt-get install locales
+    # dpkg-reconfigure locales (podemos configurar varios)
+    # locale (ver o locale por defecto)
+    # locale -a (ver os dispoñibles)
+    # para poder usar en codigo locale temos que importar a libreria import locale
+    @api.depends('data')
+    def _mes_castelan(self):
+        # O idioma por defecto é o configurado en locale na máquina onde se executa odoo.
+        # Podemos cambialo con locale.setlocale, os idiomas teñen que estar instalados na máquina onde se executa odoo.
+        # Lista onde podemos ver os distintos valores: https://docs.moodle.org/dev/Table_of_locales#Table
+        # Definimos en miñasUtilidades un metodo para asignar o distinto literal que ten o idioma en función da plataforma Windows ou GNULinux
+        locale.setlocale(locale.LC_TIME, miñasUtilidades.cadeaTextoSegunPlataforma('Spanish_Spain.1252', 'es_ES.utf8'))
+        for rexistro in self:
+            rexistro.mes_castelan = rexistro.data.strftime("%B")  # strftime https://strftime.org/
+
+    @api.depends('data')
+    def _mes_galego(self):
+        # O idioma por defecto é o configurado en locale na máquina onde se executa odoo.
+        # Podemos cambialo con locale.setlocale, os idiomas teñen que estar instalados na máquina onde se executa odoo.
+        # Lista onde podemos ver os distintos valores: https://docs.moodle.org/dev/Table_of_locales#Table
+        # Definimos en miñasUtilidades un metodo para asignar o distinto literal que ten o idioma en función da plataforma Windows ou GNULinux
+        locale.setlocale(locale.LC_TIME, miñasUtilidades.cadeaTextoSegunPlataforma('Galician_Spain.1252', 'gl_ES.utf8'))
+        for rexistro in self:
+            rexistro.mes_galego = rexistro.data.strftime("%B")
+        locale.setlocale(locale.LC_TIME, miñasUtilidades.cadeaTextoSegunPlataforma('Spanish_Spain.1252', 'es_ES.utf8'))
+
